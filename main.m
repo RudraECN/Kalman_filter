091355310905528f4bbfa8f669cb16e7f902d931
nknab <bkojo21@gmail.com>
2020-12-08 10:58:40 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,20 @@
+%{
+%File: main.m
+%Project: Autonomous Vehicles Lab | Kalman Filter
+%File Created: Thursday, 19th November 2020 8:14:28 AM
+%Authors: Rudra Patel
+%         Kojo Anyinam-Boateng
+%Emails: rudra.patel@eleves.ec-nantes.fr
+%        kojo.anyinam-boateng@eleves.ec-nantes.fr
+%Version: 1.0
+%Brief:
+%-----
+%Last Modified: Tuesday, 8th December 2020 10:58:15 AM
+%Modified By: Kojo Anyinam-Boateng
+%-----
+%Copyright ©2020 Rudra Patel & Kojo Anyinam-Boateng
+%}
+
 clear all
 close all
 clc
@@ -17,56 +34,69 @@ p1_0 = [0.1 0; 0 0];
 
 u = inputvoltage(D1, A1, Delta, Ts);
 
-[y, X] = simulates(u, G, T, Ts, L, x1);
+[y, X] = simulate(u, G, T, Ts, L, x1);
 
 %Normal kalman filter
-[X_E] = kalmann_filter(y, u, G, Tf, Ts, L, x1_0, p1_0, q);
-errortheta = X_E(1, :) -X(1, :);
+[X_E] = kal(y, u, G, Tf, Ts, L, x1_0, p1_0, q);
+thetaError = X_E(1, :) -X(1, :);
+
 figure(2)
 subplot(3, 1, 1)
 %plotting the op of kalman with the simulation
 plot ((0:Ts:D1), X_E(1, :), 'r');
+
 hold on
+
 %theta is the first element
 plot ((0:Ts:D1), X(1, :), 'b');
 ylabel('theta')
 xlabel('Time in seconds')
 title('Comparing Theta from Kalman filter and Simulation')
+
 subplot(3, 1, 2)
 plot ((0:Ts:D1), X_E(2, :), 'r');
+
 hold on
+
 %omega is the second
 plot ((0:Ts:D1), X(2, :), 'b');
 ylabel('Error')
 xlabel('Time in seconds')
 title('Comparing Omega from Kalman filter and Simulation')
+
 subplot(3, 1, 3)
-plot ((0:Ts:D1), errortheta);
+plot ((0:Ts:D1), thetaError);
 ylabel('Error')
 xlabel('Time in seconds')
 title(' y  ')
 
 %stationary Kalman filter
-[X2] = Stationary_Kalman(y, u, G, Tf, Ts, L, x1_0, p1_0, q);
+[X2] = stationarykal(y, u, G, Tf, Ts, L, x1_0, p1_0, q);
+
 figure(3)
 subplot(3, 1, 1)
 plot ((0:Ts:D1), X2(1, :), 'r');
+
 hold on
+
 plot ((0:Ts:D1), X(1, :), 'b');
 ylabel('theta')
 xlabel('Time in seconds')
 title('Comparing Theta from Stationary Kalman filter and Simulation ')
-errortheta2 = X2(1, :) - X(1, :);
+thetaError2 = X2(1, :) - X(1, :);
+
 subplot(3, 1, 2)
 plot ((0:Ts:D1), X2(2, :), 'r');
+
 hold on
+
 plot ((0:Ts:D1), X(2, :), 'b');
 ylabel('Omega')
 xlabel('Time in seconds')
 title('Comparing Omega from Stationary Kalman filter and Simulation ')
 
 subplot(3, 1, 3)
-plot ((0:Ts:D1), errortheta2);
+plot ((0:Ts:D1), thetaError2);
 ylabel('Error')
 xlabel('Time in secomds')
 title('y')
